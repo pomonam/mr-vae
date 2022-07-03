9e20a4df4ecee82a81ee2efe6f133e728b392817
pomonam <juhan.bae@mail.utoronto.ca>
2022-07-03 13:26:42 -0400

    initial commit

---
+++ models/vae.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from encoder import MLPEncoder
+from decoder import MLPDecoder
+
+
+class BetaVAE(nn.Module):
+
+  def __init__(self, encoder, decoder, beta=1.):
+    super(BetaVAE, self).__init__()
+
+    self.encoder = encoder
+    self.decoder = decoder
+
+  def encode(self, x):
+    mean, std = self.encoder(x)
+    return mean, std
+
+  def sample(self, mu, std):
+    eps = torch.randn_like(std)
+    return eps.mul(std).add_(mu)
+
+  def decode(self, z):
+    return self.decoder(z)
+
+  def forward(self, x):
+      mu, std = self.encode(x)
+      z = self.sample(mu, std)
+      rx = self.decode(z)
+      return rx, mu, std
+
