0983caf214e75fac463042dad08954571cd0e9cc
Juhan Bae <juhan.bae@mail.utoronto.ca>
2022-07-03 19:03:41 -0400

    Fix download script

--- download_data.py
+++ download_data.py
@@ -1,11 +1,10 @@
-from experiments.b_mnist_mlp.input_pipeline import build_input_queue as b_mnist_mlp_build
-
 import torch
 
 
 def main():
-    b_mnist_mlp_build("train", 1, device=torch.device("cpu"), data_path="logs/data")
-    b_mnist_mlp_build("test", 1, device=torch.device("cpu"), data_path="logs/data")
+    from experiments.b_mnist_mlp.input_pipeline import load_data
+    load_data("train", 1, data_path="logs/data")
+    load_data("test", 1, data_path="logs/data")
 
 
 if __name__ == "__main__":
