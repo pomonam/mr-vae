d3f5ece2e3889b3f503076c041fd89a89fbc48e3
pomonam <juhan.bae@mail.utoronto.ca>
2022-07-04 03:34:50 -0400

    Add reconstruction visualization

---
+++ src/schedules.py
@@ -0,0 +1,19 @@
+import torch
+import numpy as np
+
+
+def frange_cycle_linear(start, stop, n_epoch, n_cycle=4, ratio=0.5):
+  L = np.ones(n_epoch)
+  period = n_epoch / n_cycle
+  # linear schedule
+  step = (stop - start) / (period * ratio)
+
+  for c in range(n_cycle):
+
+    v, i = start, 0
+    while v <= stop and (int(i + c * period) < n_epoch):
+      L[int(i + c * period)] = v
+      v += step
+      i += 1
+  return L
+
