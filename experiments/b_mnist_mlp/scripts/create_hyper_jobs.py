a5d86bf43d5c12b52f65ce4d8c59e76dd58083b8
pomonam <juhan.bae@mail.utoronto.ca>
2022-07-05 06:02:58 -0400

    Add hyper model

---
+++ experiments/b_mnist_mlp/scripts/create_hyper_jobs.py
@@ -0,0 +1,26 @@
+import argparse
+
+import numpy as np
+
+from experiments.job_arrays import generate_job_strings
+from experiments.job_arrays import generate_sh_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file_name", type=str, default="hyper_jobs")
+parser.add_argument("--experiment_name", type=str, default="hv-b_mnist_mlp_hyper")
+
+args = parser.parse_args()
+
+CONFIG = {
+    "lr": [1e-3, 3e-4],
+    "param_method": ["mlp", "residual", "convnext"],
+}
+
+if __name__ == "__main__":
+    jobs = generate_job_strings(
+        CONFIG,
+        command_template=
+        "python hyper_train.py --experiment_name {} ".format(args.experiment_name))
+    with open(args.file_name, "w") as f:
+        f.writelines(jobs)
+    generate_sh_file(args.file_name, len(jobs))
--- src/models/vae.py
+++ src/models/vae.py
@@ -40,3 +40,42 @@ class BaseVae(nn.Module):
         outputs_dict = self.model(batch["inputs"])
         recons = outputs_dict["logits"]
         torchvision.utils.save_image(recons)
+
+
+class HyperVae(nn.Module):
+
+    def __init__(self, encoder, decoder, sampler):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.sampler = sampler
+
+    def encode(self, x, beta):
+        ix = self.encoder(x, beta)
+        mean, std = self.sampler(ix)
+        return mean, std
+
+    def sample(self, mu, std):
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z, beta):
+        return self.decoder(z, beta)
+
+    def forward(self, x, beta):
+        mu, std = self.encode(x, beta)
+        z = self.sample(mu, std)
+        rx = self.decode(z, beta)
+        outputs_dict = {
+            "inputs": x,
+            "mean": mu,
+            "stddev": std,
+            "logits": rx
+        }
+        return outputs_dict
+
+    def sample_inputs(self, batch):
+        outputs_dict = self.model(batch["inputs"])
+        recons = outputs_dict["logits"]
+        torchvision.utils.save_image(recons)
