Fix bug in loss calculation

src/models/encoders.py
@@ -1,16 +1,19 @@
 from torch import nn
+from typing import Tuple
 
-from .utils import load_activation
+from src.utils import load_activation
 
 
 class MLPEncoder(nn.Module):
-    def __init__(self, structure=(784, 70, 10), activation="relu", bias=True):
+    def __init__(self,
+                 structure: Tuple[int, ...] = (784, 70, 10),
+                 activation: str = "relu",
+                 bias: bool = True):
         super().__init__()
 
         self.structure = structure
         self.layers = nn.ModuleList([
-            nn.Linear(a, b, bias=bias)
-            for a, b in zip(structure, structure[1:])
+            nn.Linear(a, b, bias=bias) for a, b in zip(structure, structure[1:])
         ])
         self.activation_fn = load_activation(name=activation)
 
@@ -18,6 +21,5 @@ class MLPEncoder(nn.Module):
         z = z.view(-1, self.structure[0])
         for i, layer in enumerate(self.layers):
             z = layer(z, *argv)
-            if i != len(self.layers) - 1:
-                z = self.activation_fn(z)
+            z = self.activation_fn(z)
         return z

src/models/vae.py
@@ -14,32 +14,29 @@ class BaseVae(nn.Module):
 
     def encode(self, x):
         ix = self.encoder(x)
-        mean, std = self.sampler(ix)
-        return mean, std
-
-    def sample(self, mu, std):
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
+        outputs_dict = self.sampler(ix)
+        return outputs_dict
 
     def decode(self, z):
         return self.decoder(z)
 
     def forward(self, x):
-        mu, std = self.encode(x)
-        z = self.sample(mu, std)
-        rx = self.decode(z)
-        outputs_dict = {
-            "inputs": x,
-            "mean": mu,
-            "stddev": std,
-            "logits": rx
-        }
-        return outputs_dict
+        raise NotImplementedError
+        # outputs_dict = self.encode(x)
+        # z = self.sampler.sample(outputs_dict)
+        # rx = self.decode(z)
+        # outputs_dict = {
+        #     "inputs": x,
+        #     "mean": mu,
+        #     "stddev": std,
+        #     "logits": rx
+        # }
+        # return outputs_dict
 
-    def sample_inputs(self, batch):
-        outputs_dict = self.model(batch["inputs"])
-        recons = outputs_dict["logits"]
-        torchvision.utils.save_image(recons)
+    # def sample_inputs(self, batch):
+    #     outputs_dict = self.model(batch["inputs"])
+    #     recons = outputs_dict["logits"]
+    #     torchvision.utils.save_image(recons)
 
 
 class HyperVae(nn.Module):
@@ -53,12 +50,8 @@ class HyperVae(nn.Module):
 
     def encode(self, x, beta):
         ix = self.encoder(x, beta)
-        mean, std = self.sampler(ix)
-        return mean, std
-
-    def sample(self, mu, std):
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
+        outputs_dict = self.sampler(ix, beta)
+        return outputs_dict
 
     def decode(self, z, beta):
         return self.decoder(z, beta)