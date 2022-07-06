Fix bug in loss calculation

experiments/b_mnist_mlp/hyper_train.py
@@ -24,7 +24,7 @@ parser.add_argument("--experiment_name", type=str, default="hyper_vae-b_mnist_ml
 parser.add_argument("--epochs", type=int, default=200)
 parser.add_argument("--lr", type=float, default=1e-3)
 parser.add_argument("--batch_size", type=int, default=128)
-parser.add_argument("--param_method", type=str, default="convnext")
+parser.add_argument("--param_method", type=str, default="mlp")
 
 parser.add_argument("--no_cuda", type=bool, default=False)
 parser.add_argument("--seed", type=int, default=0)
@@ -94,7 +94,8 @@ def hyper_train(model, optimizer, criterion):
 
         for batch in p_bar:
             inputs = batch["inputs"]
-            betas = torch.FloatTensor(inputs.shape[0], 1).uniform_(0.001, 10).to(DEVICE)
+            betas = torch.FloatTensor(inputs.shape[0], 1).uniform_(-3, 1).to(DEVICE)
+            betas = torch.pow(10, betas)
             output_dict = model(inputs, betas)
             loss, loss_dict = criterion(output_dict, betas)
             optimizer.zero_grad()

experiments/b_mnist_mlp/model_pipeline.py
@@ -11,24 +11,15 @@ from src.models.hyper import replace_module
 
 
 class BinarizedMnistMlpModel(BaseVae):
-    def encode(self, x):
-        ix = self.encoder(x)
-        mean, std = self.sampler(ix)
-        return mean, std
-
-    def sample(self, mu, std):
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
-
     def forward(self, x):
-        mu, std = self.encode(x)
-        z = self.sample(mu, std)
-        rx = self.decode(z)
+        outputs_dict = self.encode(x)
+        z = self.sampler.sample(outputs_dict)
+        logits = self.decode(z)
         outputs_dict = {
             "inputs": x,
-            "mean": mu,
-            "stddev": std,
-            "logits": rx
+            "mean": outputs_dict["mean"],
+            "stddev": outputs_dict["stddev"],
+            "logits": logits
         }
         return outputs_dict
 
@@ -44,24 +35,15 @@ class HyperBinarizedMnistMlpModel(HyperVae):
         replace_module(self.decoder, block_name)
         replace_module(self.sampler, block_name)
 
-    def encode(self, x, beta):
-        ix = self.encoder(x, beta)
-        mean, std = self.sampler(ix, beta)
-        return mean, std
-
-    def sample(self, mu, std):
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
-
     def forward(self, x, beta):
-        mu, std = self.encode(x, beta)
-        z = self.sample(mu, std)
-        rx = self.decode(z, beta)
+        outputs_dict = self.encode(x, beta)
+        z = self.sampler.sample(outputs_dict)
+        logits = self.decode(z, beta)
         outputs_dict = {
             "inputs": x,
-            "mean": mu,
-            "stddev": std,
-            "logits": rx
+            "mean": outputs_dict["mean"],
+            "stddev": outputs_dict["stddev"],
+            "logits": logits
         }
         return outputs_dict
 
@@ -87,17 +69,13 @@ def build_hyper_model(block_name, device):
 
 
 class BinarizedMnistMlpCriterion(nn.Module):
-    # def __init__(self, beta):
-    #     super().__init__()
-    #     self.beta = beta
-
     def get_metric_lst(self):
         return ["loss", "rate", "distortion"]
 
     def forward(self, outputs_dict, beta):
         log_likelihood = -binary_cross_entropy(outputs_dict["inputs"], outputs_dict["logits"])
         kl = kl_gaussian(outputs_dict["mean"], torch.square(outputs_dict["stddev"]))
-        elbo = log_likelihood - beta * kl
+        elbo = log_likelihood - beta.view(-1) * kl
         loss_dict = {
             "loss": -torch.mean(elbo).item(),
             "distortion": -torch.mean(log_likelihood).item(),

experiments/b_mnist_mlp/results/hyper_rd_curve.py
@@ -8,7 +8,7 @@ EXPERIMENT_NAME = "hv-b_mnist_mlp_hyper"
 ID = "17f36ab6"
 
 
-def get_summary(config, summary):
+def get_summary(summary):
     beta_to_rate = {}
     beta_to_dist = {}
     beta_lst = np.logspace(-3, 1, num=20)
@@ -34,7 +34,7 @@ def main():
                  if not k.startswith('_')})
             name_list.append(run.name)
 
-    rate_dict, dist_dict = get_summary(config_list[0], summary_list[0])
+    rate_dict, dist_dict = get_summary(summary_list[0])
 
     dist_dict_sorted = {i: dist_dict[i] for i in rate_dict.keys()}
     keys = rate_dict.keys()

experiments/b_mnist_mlp/train.py
@@ -21,22 +21,20 @@ from experiments.utils import seed_everything
 parser = argparse.ArgumentParser()
 parser.add_argument("--experiment_name", type=str, default="hyper_vae-b_mnist_mlp")
 
-parser.add_argument("--epochs", type=int, default=200)
+parser.add_argument("--epochs", type=int, default=5)
 parser.add_argument("--lr", type=float, default=1e-3)
 parser.add_argument("--batch_size", type=int, default=128)
 parser.add_argument("--beta", type=float, default=1.)
-parser.add_argument("--schedule", type=str, default="cyclic")
+parser.add_argument("--schedule", type=str, default="constant")
 
-parser.add_argument("--no_cuda", type=bool, default=False)
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--checkpoint_dir", type=str, default=None)
 parser.add_argument("--save_freq", type=int, default=100)
 parser.add_argument("--eval_freq", type=int, default=10)
 args = parser.parse_args()
 
-cuda = torch.cuda.is_available() and not args.no_cuda
+cuda = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if cuda else "cpu")
-cudnn.benchmark = True
 
 
 def evaluate(model, criterion, epoch, name):
@@ -141,7 +139,6 @@ def main():
     logits = outputs_dict["logits"].view(-1, 28, 28)
     plt.figure(figsize=(5, 5))
     plt.axis("square")
-    plt.legend(frameon=True)
     for i in range(50):
         data_i = test_batch["inputs"].view(-1, 28, 28)[i].data.cpu().numpy()
         recon_i = torch.sigmoid(logits[i]).data.cpu().numpy()

src/models/blocks.py
@@ -7,12 +7,14 @@ class MlpBlock(nn.Module):
     def __init__(self, width: int, rank: int = None):
         super().__init__()
         self.width = width
-        self.mid_width = width * 4 if rank is None else rank
+        self.mid_width = width * 2 if rank is None else rank
 
         self.net = nn.Sequential(
             nn.Linear(1, self.mid_width),
             nn.GELU(),
-            nn.Linear(self.mid_width, self.width)
+            nn.Linear(self.mid_width, self.mid_width),
+            nn.GELU(),
+            nn.Linear(self.mid_width, self.mid_width)
         )
         self._init_weights()
 
@@ -24,7 +26,13 @@ class MlpBlock(nn.Module):
                     module.bias.data.normal_(std=1e-6)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.net(x)
+        out = self.net(x)
+        scale, bias = out[:, :self.width], out[:, self.width:]
+        outputs_dict = {
+            "scale": scale,
+            "bias": bias
+        }
+        return outputs_dict
 
 
 class ResidualBlock(nn.Module):

src/models/decoders.py
@@ -1,9 +1,12 @@
 from torch import nn
-from .utils import load_activation
-
+from src.utils import load_activation
+from typing import Tuple
 
 class MLPDecoder(nn.Module):
-    def __init__(self, structure=(784, 70, 10), activation="relu", bias=True):
+    def __init__(self,
+                 structure: Tuple[int, ...] = (784, 70, 10),
+                 activation: str = "relu",
+                 bias: bool = True):
         super().__init__()
         self.structure = structure
         self.layers = nn.ModuleList([

src/models/hyper.py
@@ -1,7 +1,5 @@
 from torch import nn
 from src.models.blocks import MlpBlock, ResidualBlock, BatchNormResidualBlock, ConvNextBlock
-from src.utils import set_attr, del_attr
-import torch
 
 _BLOCK_DICT = {
   "bn_residual": BatchNormResidualBlock,
@@ -11,29 +9,17 @@ _BLOCK_DICT = {
 }
 
 
-# def replace_module(module, block_name):
-#     for attr_str in dir(module):
-#         target_attr = getattr(module, attr_str)
-#         if type(target_attr) == torch.nn.Linear:
-#             # print('replaced: ', name, attr_str)
-#             new_bn = HyperModule(module, module.out_features, block_name)
-#             setattr(module, attr_str, new_bn)
-#
-#     for name, immediate_child_module in module.named_children():
-#         replace_module(immediate_child_module, name)
-
-
 def replace_module(model: nn.Module, block_name) -> None:
     for name, module in model.named_children():
         if len(list(module.children())) > 0:
             replace_module(module, block_name)
 
         if isinstance(module, nn.Linear):
-            # del_attr(model, name)
             hyper_module = HyperModule(module, module.out_features, block_name)
             setattr(model, name, hyper_module)
 
         if isinstance(module, nn.Conv2d):
+            # TODO: Change this
             hyper_module = HyperModule(module, block_name)
             setattr(model, name, hyper_module)
 
@@ -41,13 +27,20 @@ def replace_module(model: nn.Module, block_name) -> None:
 class HyperModule(nn.Module):
     def __init__(self,
                  module: nn.Module,
+                 # hyper_type: str,
                  width,
                  block_name: str):
         super().__init__()
         self.module = module
+        self.hyper_type = "cond"
         self.hyper_module = _BLOCK_DICT[block_name](width)
 
     def forward(self, x, beta):
         x = self.module(x)
-        x = x + self.hyper_module(beta)
+
+        if self.hyper_type == "cond":
+            outputs_dict = self.hyper_module(beta)
+            x = x * outputs_dict["scale"] + outputs_dict["bias"]
+        else:
+            x = x + self.hyper_module(beta)
         return x

src/models/samplers.py
@@ -7,11 +7,12 @@ class BaseSampler(nn.Module):
     def forward(self, x):
         raise NotImplementedError
 
-    def sample(self, x):
+    @staticmethod
+    def sample(outputs_dict):
         raise NotImplementedError
 
 
-class IsotropicGaussianSampler(nn.Module):
+class IsotropicGaussianSampler(BaseSampler):
     def __init__(self, hidden_size, latent_size, bias=True):
         super().__init__()
 
@@ -22,4 +23,13 @@ class IsotropicGaussianSampler(nn.Module):
         mean = self.mean(x, *argv)
         log_stddev = self.log_stddev(x, *argv)
         stddev = torch.exp(log_stddev)
-        return mean, stddev
+        outputs_dict = {
+            "mean": mean,
+            "stddev": stddev
+        }
+        return outputs_dict
+
+    @staticmethod
+    def sample(outputs_dict):
+        eps = torch.randn_like(outputs_dict["stddev"])
+        return eps.mul(outputs_dict["stddev"]).add_(outputs_dict["mean"])