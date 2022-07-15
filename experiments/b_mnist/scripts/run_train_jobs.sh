edea824e548e612f8c81480553770b32547aa110
Juhan Bae <juhan.bae@mail.utoronto.ca>
2022-07-15 17:40:00 -0400

    Fix hyper scripts

--- experiments/b_mnist/scripts/run_train_jobs.sh
+++ experiments/b_mnist/scripts/run_train_jobs.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=t4v1,p100,t4v2,rtx6000
 #SBATCH --qos=normal
 #SBATCH --export=ALL
-#SBATCH --array=0-62%62
+#SBATCH --array=0-63%63
 #SBATCH --output=temp/array-%A_%a.out
 #SBATCH -c 4
 
