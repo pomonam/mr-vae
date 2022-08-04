df2b2f6a59600c18024308bcfd978b4d281b2a96
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-04 10:19:01 -0400

    Add chunking type again

--- experiments/b_mnist/scripts/create_hyper_jobs.py
+++ experiments/b_mnist/scripts/create_hyper_jobs.py
@@ -7,12 +7,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--file_name", type=str, default="hyper_jobs")
 parser.add_argument("--experiment_name",
                     type=str,
-                    default="hv-b_mnist_mlp_hyper-v23")
+                    default="hv-b_mnist_mlp_hyper-v24")
 
 args = parser.parse_args()
 
 CONFIG = {
-    "lr": [1e-4],
+    "lr": [1e-3, 1e-4],
     "total_epochs": [200, 400],
     "encoder_name": ["mlp"],
     "decoder_name": ["mlp"],
