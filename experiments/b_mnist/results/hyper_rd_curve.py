901adf9a633c13165ce719f42c8a86e969e5a8a6
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-10 23:44:10 -0400

    increase ss

--- experiments/b_mnist/results/hyper_rd_curve.py
+++ experiments/b_mnist/results/hyper_rd_curve.py
@@ -6,8 +6,8 @@ from experiments.init_wandb import init_api
 from src.plotting import init_plotting
 
 ENTITY = "bae-group"
-EXPERIMENT_NAME = "hv-b_mnist_mlp_hyper-v63"
-ID = "3o9yje9g"
+EXPERIMENT_NAME = "hv-b_mnist_mlp_hyper-v64"
+ID = "l0axw5yk"
 
 
 def get_summary(summary):
