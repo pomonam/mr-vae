4b5352b93e8090c990eb63155144a9fdb78f4fc0
Juhan Bae <juhan.bae@mail.utoronto.ca>
2022-08-15 14:00:12 -0400

    Bug fix on sampler

--- tests/theory/linear_train.py
+++ tests/theory/linear_train.py
@@ -1,19 +1,23 @@
+import argparse
+from math import pi
+import os
+
+from linear_utils import analytic_rate_and_distortion
+from linear_vae import LinearDecoder
+from linear_vae import LinearEncoder
+from linear_vae import LinearHyperVae
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from torch import nn
-import argparse
-import numpy as np
-import wandb
 import tqdm
-import os
-from math import pi
-import matplotlib.pyplot as plt
+import wandb
 
 from experiments.b_mnist.input_pipeline import build_input_queue
 from experiments.b_mnist.model_pipeline import build_criterion
 from experiments.b_mnist.results.rd_curve import get_rd
 from experiments.init_wandb import init_api
 from experiments.init_wandb import init_wandb
-
 from src.config import HyperConfig
 from src.config import TrainConfig
 from src.evaluate import generate_metric_str
@@ -23,14 +27,15 @@ from src.evaluate import update_metric
 from src.plotting import init_plotting
 from src.utils import seed_everything
 
-from linear_vae import LinearHyperVae, LinearEncoder, LinearDecoder
-from linear_utils import analytic_rate_and_distortion
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--experiment_name", type=str, default="hyper_vae-hyper-b_mnist_linear")
+parser.add_argument(
+    "--experiment_name", type=str, default="hyper_vae-hyper-b_mnist_linear")
 
-parser.add_argument("--training_method", type=str, default="sequential",
-                    choices=["simultaneous", "sequential"])
+parser.add_argument(
+    "--training_method",
+    type=str,
+    default="sequential",
+    choices=["simultaneous", "sequential"])
 parser.add_argument("--bottleneck_size", type=int, default=128)
 parser.add_argument("--hyper_type", type=str, default="add")
 parser.add_argument("--block_type", type=str, default="linear")
@@ -62,13 +67,19 @@ ENTITY = "lrscheduler114"
 EXPERIMENT_NAME = "hyper_vae-hyper-b_mnist_linear"
 ID = "k9m4y9yx"
 
+
 def get_summary(summary):
-    beta_to_rate = dict(zip(summary["train_eval/beta_lst"], summary["train_eval/rate_lst"]))
-    beta_to_dist = dict(zip(summary["train_eval/beta_lst"], summary["train_eval/dist_lst"]))
-    beta_to_elbo = dict(zip(summary["train_eval/beta_lst"], summary["train_eval/loss_lst"]))
-    
-    beta_to_rate_analytical = dict(zip(summary["analytical/beta_lst"], summary["analytical/rate_lst"]))
-    beta_to_dist_analytical = dict(zip(summary["analytical/beta_lst"], summary["analytical/dist_lst"]))
+    beta_to_rate = dict(
+        zip(summary["train_eval/beta_lst"], summary["train_eval/rate_lst"]))
+    beta_to_dist = dict(
+        zip(summary["train_eval/beta_lst"], summary["train_eval/dist_lst"]))
+    beta_to_elbo = dict(
+        zip(summary["train_eval/beta_lst"], summary["train_eval/loss_lst"]))
+
+    beta_to_rate_analytical = dict(
+        zip(summary["analytical/beta_lst"], summary["analytical/rate_lst"]))
+    beta_to_dist_analytical = dict(
+        zip(summary["analytical/beta_lst"], summary["analytical/dist_lst"]))
     return beta_to_rate, beta_to_dist, beta_to_elbo, beta_to_rate_analytical, beta_to_dist_analytical
 
 
@@ -83,8 +94,7 @@ def plot_rd_curves():
         if run.state == "finished" and run.id == ID:
             summary_list.append(run.summary._json_dict)
             config_list.append(
-                {k: v for k, v in run.config.items()
-                 if not k.startswith('_')})
+                {k: v for k, v in run.config.items() if not k.startswith('_')})
             name_list.append(run.name)
 
     results = get_summary(summary_list[0])
@@ -101,17 +111,26 @@ def plot_rd_curves():
     plt.scatter(rate, dist, facecolors="none", edgecolors="k")
     plt.scatter(rate, dist)
 
-
     keys = analytical_rate_dict.keys()
     values = zip(analytical_rate_dict.values(), analytical_dist_dict.values())
     analytical_combined_dict = dict(zip(keys, values))
 
-    analytical_rate = np.array([c[0] for c in analytical_combined_dict.values()])
-    analytical_dist = np.array([c[1] for c in analytical_combined_dict.values()])
+    analytical_rate = np.array(
+        [c[0] for c in analytical_combined_dict.values()])
+    analytical_dist = np.array(
+        [c[1] for c in analytical_combined_dict.values()])
     plt.plot(analytical_rate, analytical_dist, label='Analytical')
 
-    min_val = min(np.min(rate), np.min(dist), np.min(analytical_rate), np.min(analytical_dist)) - 10
-    max_val = min(np.max(rate), np.max(dist), np.max(analytical_rate), np.max(analytical_dist)) + 10
+    min_val = min(
+        np.min(rate),
+        np.min(dist),
+        np.min(analytical_rate),
+        np.min(analytical_dist)) - 10
+    max_val = min(
+        np.max(rate),
+        np.max(dist),
+        np.max(analytical_rate),
+        np.max(analytical_dist)) + 10
     plt.xlim(min_val, max_val)
     plt.ylim(min_val, max_val)
 
@@ -130,6 +149,7 @@ def plot_rd_curves():
 # Hypernetwork Training
 # =====================
 
+
 def hyper_evaluate(model, criterion, epoch, name):
     model.eval()
 
@@ -138,7 +158,6 @@ def hyper_evaluate(model, criterion, epoch, name):
         loss_lst = []
         rate_lst = []
         dist_lst = []
-
 
         if name == "analytical":
             for beta in tqdm.tqdm(beta_lst):
@@ -159,7 +178,9 @@ def hyper_evaluate(model, criterion, epoch, name):
                     # We want to compute exact ELBO here
                     _, loss_dict = criterion.eval_forward(output_dict)
 
-                    metric_dict = update_metric(metric_dict, loss_dict, inputs.size(0))
+                    metric_dict = update_metric(metric_dict,
+                                                loss_dict,
+                                                inputs.size(0))
                     summ_dict = summarize_metric(metric_dict)
                     summ_str = generate_metric_str(name, epoch, summ_dict)
                     p_bar.set_description(summ_str)
@@ -178,8 +199,10 @@ def hyper_evaluate(model, criterion, epoch, name):
 
         rd_data = [[x, y] for (x, y) in zip(rate_lst, dist_lst)]
         table = wandb.Table(data=rd_data, columns=["rate", "distortion"])
-        wandb.log({f"{name}/rd_curve":
-                   wandb.plot.line(table, "rate", "distortion", title="RD Curve")})
+        wandb.log({
+            f"{name}/rd_curve":
+                wandb.plot.line(table, "rate", "distortion", title="RD Curve")
+        })
 
         loss_lst = np.array(loss_lst)
         rate_lst = np.array(rate_lst)
@@ -198,15 +221,16 @@ def hyper_evaluate(model, criterion, epoch, name):
             f"{name}/max_dist": np.max(dist_lst),
             f"{name}/min_dist": np.min(dist_lst),
             f"{name}/abs_dist": np.max(dist_lst) - np.min(dist_lst),
-
         }
         wandb.log(auc_dict)
 
 
 def hyper_train(model, biq, criterion, optimizer, cfg, hyper_cfg):
     do_checkpoint = cfg.checkpoint_dir is not None
-    if do_checkpoint and os.path.exists(os.path.join(args.checkpoint_dir, "checkpoint.pth")):
-        slurm_checkpoint = torch.load(os.path.join(args.checkpoint_dir, "checkpoint.pth"))
+    if do_checkpoint and os.path.exists(
+            os.path.join(args.checkpoint_dir, "checkpoint.pth")):
+        slurm_checkpoint = torch.load(
+            os.path.join(args.checkpoint_dir, "checkpoint.pth"))
         model.load_state_dict(slurm_checkpoint["state_dict"])
         optimizer.load_state_dict(slurm_checkpoint["optimizer"])
         epoch = slurm_checkpoint["epoch"]
@@ -222,7 +246,8 @@ def hyper_train(model, biq, criterion, optimizer, cfg, hyper_cfg):
             hyper_evaluate(model, criterion, epoch, "test")
 
         if do_checkpoint and do_save:
-            slurm_check_dir = os.path.join(args.checkpoint_dir, "checkpoint.pth")
+            slurm_check_dir = os.path.join(args.checkpoint_dir,
+                                           "checkpoint.pth")
             log_info = {
                 "id": wandb.run.id,
                 "epoch": epoch,
@@ -259,7 +284,10 @@ def hyper_train(model, biq, criterion, optimizer, cfg, hyper_cfg):
 
 
 def main():
-    init_wandb(args.checkpoint_dir, project_name=args.experiment_name, config=vars(args))
+    init_wandb(
+        args.checkpoint_dir,
+        project_name=args.experiment_name,
+        config=vars(args))
     cfg = TrainConfig(args)
     hyper_cfg = HyperConfig(args)
 
@@ -275,6 +303,7 @@ def main():
     hyper_evaluate(model, criterion, cfg.total_epochs, "analytical")
     hyper_evaluate(model, criterion, cfg.total_epochs, "train_eval")
     hyper_evaluate(model, criterion, cfg.total_epochs, "test")
+
 
 if __name__ == "__main__":
     main()
--- tests/theory/linear_vae.py
+++ tests/theory/linear_vae.py
@@ -1,7 +1,9 @@
-import torch
-import torch.nn as nn
 import math
 from math import pi
+
+import torch
+import torch.nn as nn
+
 from src.config import HyperConfig
 from src.hyper.layers.linear import HyperLinear
 from src.hyper.layers.module import HyperModule
@@ -15,6 +17,7 @@ DEVICE = torch.device("cuda" if cuda else "cpu")
 # Linear VAE Class Adapted From
 # GitHub: https://github.com/BorealisAI/rate_distortion/blob/master/rate_distortion/models/vaes.py
 # ================================================================================================
+
 
 def log_normal_likelihood(x, mean, logvar):
     """Implementation WITH constant
@@ -50,8 +53,8 @@ def log_normal(x, mean, logvar):
     Returns:
         output: [B]
     """
-    return -0.5 * (logvar.sum(1) + (
-        (x - mean).pow(2) / torch.exp(logvar)).sum(1))
+    return -0.5 * (
+        logvar.sum(1) + ((x - mean).pow(2) / torch.exp(logvar)).sum(1))
 
 
 def singleton_repeat(x, n):
@@ -71,22 +74,24 @@ def singleton_repeat(x, n):
 
 
 class LinearEncoder(nn.Module):
+
     def __init__(self, bottleneck_size):
         super().__init__()
         self.bottleneck_size = bottleneck_size
-        self.layer = nn.Linear(784, bottleneck_size*2, bias=False)
-    
+        self.layer = nn.Linear(784, bottleneck_size * 2, bias=False)
+
     def forward(self, x):
         x = x.reshape(-1, 784)
         return self.layer(x)
 
 
 class LinearDecoder(nn.Module):
+
     def __init__(self, bottleneck_size):
         super().__init__()
         self.bottleneck_size = bottleneck_size
         self.layer = nn.Linear(bottleneck_size, 784, bias=False)
-    
+
     def forward(self, x):
         x = x.reshape(-1, self.bottleneck_size)
         return self.layer(x)
@@ -100,7 +105,8 @@ class LinearVae(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
         self.observation_log_likelihood_fn = log_normal_likelihood
-        self.x_logvar = nn.Parameter(torch.log(torch.tensor(1)), requires_grad=True)
+        self.x_logvar = nn.Parameter(
+            torch.log(torch.tensor(1)), requires_grad=True)
 
     def encode(self, x):
         hidden = self.encoder(x)
@@ -127,8 +133,9 @@ class LinearVae(nn.Module):
         z, logpz, logqz = self.reparameterize(mu, logvar)
         x_mean, x_logvar = self.decode(z)
 
-        likelihood = self.observation_log_likelihood_fn(
-            flattened_x_k, x_mean, x_logvar)
+        likelihood = self.observation_log_likelihood_fn(flattened_x_k,
+                                                        x_mean,
+                                                        x_logvar)
         elbo = likelihood + logpz - logqz
 
         if num_iwae != 1:
@@ -149,9 +156,11 @@ class LinearVae(nn.Module):
         }
         return elbo, output_dict
 
+
 # ==============================
 # === Hyper Linear-VAE Class ===
 # ==============================
+
 
 def replace_module(model: nn.Module, hyper_config: HyperConfig) -> None:
     for name, module in model.named_children():
@@ -162,7 +171,8 @@ def replace_module(model: nn.Module, hyper_config: HyperConfig) -> None:
             hyper_module = HyperLinear(module, hyper_config)
             setattr(model, name, hyper_module)
 
-        if isinstance(module, nn.Conv2d) or isinstance(module, nn.ConvTranspose2d):
+        if isinstance(module, nn.Conv2d) or isinstance(module,
+                                                       nn.ConvTranspose2d):
             hyper_module = HyperModule(module, hyper_config)
             setattr(model, name, hyper_module)
 
@@ -209,22 +219,27 @@ class LinearHyperVae(LinearVae):
         if self.hyper_config.sample_type == "log_uniform":
             a = math.log(a)
             b = math.log(b)
-            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).uniform_(a, b).to(device)
+            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).uniform_(
+                a, b).to(device)
             sample_dict["trans_beta"] = torch.exp(sample_dict["net_beta"])
 
         elif self.hyper_config.sample_type == "fixed_log_uniform":
             cons = math.sqrt(3)
-            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).uniform_(-cons, cons).to(device)
+            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).uniform_(
+                -cons, cons).to(device)
             # Equivalent to setting a = -3 and b = 1
             norm_beta = sample_dict["net_beta"] * (2 * cons) / 3
             sample_dict["trans_beta"] = torch.pow(10, norm_beta - 1)
 
         elif self.hyper_config.sample_type == "fixed_normal":
-            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).normal_(mean=0, std=1).to(device)
-            sample_dict["trans_beta"] = stretch_sigmoid(sample_dict["net_beta"] - 2, low=1e-3, high=10, slope=2)
+            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).normal_(
+                mean=0, std=1).to(device)
+            sample_dict["trans_beta"] = stretch_sigmoid(
+                sample_dict["net_beta"] - 2, low=1e-3, high=10, slope=2)
 
         elif self.hyper_config.sample_type == "uniform":
-            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).uniform_(a, b).to(device)
+            sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).uniform_(
+                a, b).to(device)
             sample_dict["trans_beta"] = torch.exp(sample_dict["net_beta"])
 
         else:
@@ -248,7 +263,8 @@ class LinearHyperVae(LinearVae):
 
         elif self.hyper_config.sample_type == "fixed_normal":
             beta = ones * beta
-            trans_beta = stretch_sigmoid_inv(beta, low=1e-3, high=10, slope=2) + 2
+            trans_beta = stretch_sigmoid_inv(
+                beta, low=1e-3, high=10, slope=2) + 2
 
         elif self.hyper_config.sample_type == "normal":
             # sample_dict["net_beta"] = torch.FloatTensor(batch_size, 1).normal_(0, std=1).to(device)
@@ -286,4 +302,3 @@ class LinearHyperVae(LinearVae):
         elbo, output_dict = self.forward(x)
         output_dict["beta"] = torch.zeros(zero_beta.shape)
         return output_dict
-
