0f8bc42a13f5eca8ada873d497ca1d0fee6da612
Juhan Bae <juhan.bae@mail.utoronto.ca>
2022-08-15 13:32:10 -0400

    Add script

--- experiments/b_mnist/scripts/create_hyper_jobs.py
+++ experiments/b_mnist/scripts/create_hyper_jobs.py
@@ -6,7 +6,7 @@ from experiments.job_arrays import generate_sh_file
 parser = argparse.ArgumentParser()
 parser.add_argument("--file_name", type=str, default="hyper_jobs")
 parser.add_argument(
-    "--experiment_name", type=str, default="hypvae-mnist_mlp_hyper-v6")
+    "--experiment_name", type=str, default="hypvae-mnist_mlp_hyper-v7")
 
 args = parser.parse_args()
 
@@ -15,11 +15,12 @@ CONFIG = {
     "total_epochs": [400],
     "encoder_name": ["mlp"],
     "decoder_name": ["mlp"],
-    "block_type": ["linear", "mlp"],
+    "block_type": ["linear"],
     "sample_type": ["beta_log_uniform"],
     "preact_transform": [0, 1],
-    "preprocess_beta": [1],
-    "include_sigmoid_activation": [1],  # "include_layer_norm": [0, 1],
+    "preprocess_beta": [0],
+    "include_sigmoid_activation": [1],
+    "include_layer_norm": [0, 1],
     "include_residual_connection": [0, 1],
     "include_chunk": [0, 1],
 }
