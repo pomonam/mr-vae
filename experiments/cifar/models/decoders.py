694f9e42db4b79403cdac32001eee944264c490d
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-17 01:40:18 -0400

    Fix cifar architecture

--- experiments/cifar/models/decoders.py
+++ experiments/cifar/models/decoders.py
@@ -11,24 +11,17 @@ class ResNetDecoder(BaseDecoder):
         super().__init__()
 
         self.first_proj = nn.Linear(64, 128 * 8 * 8)
-        layers = nn.Sequential()
-        layers.append(
-          nn.Sequential(
-            ResBlock(in_channels=128, out_channels=32),
-            ResBlock(in_channels=128, out_channels=32),
-          )
-        )
-        layers.append(nn.Sequential(nn.ConvTranspose2d(128, 64, 4, 2, padding=1), nn.ReLU()))
-        layers.append(
-          nn.Sequential(
-            nn.ConvTranspose2d(64, 3, 4, 2, padding=1)
-          )
+        layers = nn.Sequential(
+          ResBlock(in_channels=128, out_channels=32),
+          ResBlock(in_channels=128, out_channels=32),
+          nn.ConvTranspose2d(128, 64, 4, 2, padding=1),
+          nn.ReLU(),
+          nn.ConvTranspose2d(64, 3, 4, 2, padding=1)
         )
         self.layers = layers
 
     def forward(self, z):
         z = self.first_proj(z)
-        z = torch.relu(z)
         z = z.reshape(z.shape[0], 128, 8, 8)
         z = self.layers(z)
         return z
--- experiments/cifar/models/encoders.py
+++ experiments/cifar/models/encoders.py
@@ -12,18 +12,15 @@ class ResNetEncoder(BaseEncoder):
     def __init__(self):
         super().__init__()
 
-        layers = nn.Sequential()
-        layers.append(nn.Sequential(nn.Conv2d(3, 64, 4, 2, padding=1), nn.ReLU()))
-        layers.append(nn.Sequential(nn.Conv2d(64, 128, 4, 2, padding=1), nn.ReLU()))
-        layers.append(nn.Sequential(nn.Conv2d(128, 128, 3, 1, padding=1)))
-
-        layers.append(
-          nn.Sequential(
-            ResBlock(in_channels=128, out_channels=32),
-            ResBlock(in_channels=128, out_channels=32),
-          )
+        layers = nn.Sequential(
+          nn.Conv2d(3, 64, 4, 2, padding=1),
+          nn.ReLU(),
+          nn.Conv2d(64, 128, 4, 2, padding=1),
+          nn.ReLU(),
+          nn.Conv2d(128, 128, 3, 1, padding=1),
+          ResBlock(in_channels=128, out_channels=32),
+          ResBlock(in_channels=128, out_channels=32)
         )
-
         self.layers = layers
 
     def forward(self, x):
