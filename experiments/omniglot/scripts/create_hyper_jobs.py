9644e9f20ed291c61fa4e54efcd5f5190b85da5b
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-23 04:05:43 -0400

    Reduce SS and add HyperLinear when sampling

--- experiments/omniglot/scripts/create_hyper_jobs.py
+++ experiments/omniglot/scripts/create_hyper_jobs.py
@@ -5,7 +5,7 @@ from experiments.job_arrays import generate_sh_file
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file_name", type=str, default="hyper_jobs")
-parser.add_argument("--experiment_name", type=str, default="hypervae_omniglot_hyper_train_v1")
+parser.add_argument("--experiment_name", type=str, default="hypervae_omniglot_hyper_train_v2")
 
 
 args = parser.parse_args()
