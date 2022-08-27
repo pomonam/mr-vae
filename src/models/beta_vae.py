bb809e5e1e81c30854577992b3f72c9c995755d0
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-27 02:45:20 -0400

    Add refactored mnist

---
+++ src/models/beta_vae.py
@@ -0,0 +1,51 @@
+import os
+from typing import Optional
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+# from ...data.datasets import BaseDataset
+# from ..base.base_utils import ModelOutput
+# from ..nn import BaseDecoder, BaseEncoder
+# from ..vae import VAE
+# from .beta_vae_config import BetaVAEConfig
+
+
+# class BetaVAE(VAE):
+class BetaVAE(nn.Module):
+
+    def __init__(
+        self,
+        encoder = None,
+        decoder = None,
+    ):
+        super().__init__()
+        # BetaVAE.__init__(self, encoder=encoder, decoder=decoder)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.model_name = "BetaVAE"
+
+    def forward(self, inputs, **kwargs):
+        x = inputs["data"]
+
+        encoder_output = self.encoder(x)
+
+        mu, log_var = encoder_output["embedding"], encoder_output["log_covariance"]
+
+        std = torch.exp(0.5 * log_var)
+        z, eps = self._sample_gauss(mu, std)
+        recon_x = self.decoder(z)["reconstruction"]
+
+        output = {
+            "reconstruction": recon_x,
+            "data": x,
+            "mu": mu,
+            "log_var": log_var,
+            "z": z,
+        }
+
+        return output
+
+    def _sample_gauss(self, mu, std):
+        eps = torch.randn_like(std)
+        return mu + eps * std, eps
