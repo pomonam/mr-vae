66f95509d783943a016c91500c48462a17f1044b
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-29 05:31:14 -0400

    Add text

---
+++ experiments/image/sweep_scripts/create_baseline_sweep.py
@@ -0,0 +1,47 @@
+import argparse
+
+from experiments.array_utils import generate_job_strings
+from experiments.array_utils import generate_sh_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file_name", type=str, default="baseline_sweep")
+parser.add_argument(
+    "--experiment_name",
+    type=str,
+    default="hv_image_sweep")
+
+args = parser.parse_args()
+
+CONV_CONFIG = {
+    "lr": [1e-3, 3e-4, 1e-4, 3e-5, 1e-5],
+    "total_epochs": [100],
+    "data_name": ["cifar", "svhn", "celeba"],
+    "encoder_name": ["conv"],
+    "decoder_name": ["conv"],
+    "schedule": ["monotonic"],
+    "beta": [1.]
+}
+
+RENSET_CONFIG = {
+    "lr": [1e-3, 3e-4, 1e-4, 3e-5, 1e-5],
+    "total_epochs": [100],
+    "data_name": ["cifar", "svhn", "celeba"],
+    "encoder_name": ["resnet"],
+    "decoder_name": ["resnet"],
+    "schedule": ["monotonic"],
+    "beta": [1.]
+}
+
+if __name__ == "__main__":
+  jobs = generate_job_strings(
+      CONV_CONFIG,
+      command_template="python baseline_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  jobs += ["\n"]
+  jobs += generate_job_strings(
+      RENSET_CONFIG,
+      command_template="python baseline_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  with open(args.file_name, "w") as f:
+    f.writelines(jobs)
+  generate_sh_file(args.file_name, len(jobs))
