fa7b2e5d65518cdd4ba6ee11cc41f25a9fed7c7a
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-28 20:05:44 -0400

    add omniglot

--- experiments/third_party/scripts/generate_binary_jobs.py
+++ experiments/third_party/scripts/generate_binary_jobs.py
@@ -4,21 +4,22 @@ from experiments.array_utils import generate_job_strings
 from experiments.array_utils import generate_sh_file
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file_name", type=str, default="baseline_jobs")
+parser.add_argument("--file_name", type=str, default="baseline_binary_jobs")
 parser.add_argument(
   "--experiment_name",
   type=str,
-  default="nvae_binary_image_train_baseline")
+  default="nvae_binary_train_baseline")
 
 args = parser.parse_args()
 
 CONFIG = {
+  "dataset": ["mnist", "omniglot"],
   "kl_fixed": np.logspace(-3, 1, num=20),
 }
 
 
 if __name__ == "__main__":
-  base_str = "python new_train.py --data ../../../logs/data --root checkpoints/ --dataset mnist --batch_size 200 " \
+  base_str = "python new_train.py --data ../../../logs/data --root checkpoints/ --batch_size 200 " \
              "--epochs 400 --num_latent_scales 2 --num_groups_per_scale 10 --num_postprocess_cells 3 --num_preprocess_cells 3 " \
              "--num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 --num_latent_per_group 20 --num_preprocess_blocks 2 " \
              "--num_postprocess_blocks 2 --weight_decay_norm 1e-2 --num_channels_enc 32 --num_channels_dec 32 --num_nf 0 " \
@@ -29,4 +30,4 @@ if __name__ == "__main__":
       args.experiment_name))
   with open(args.file_name, "w") as f:
     f.writelines(jobs)
-  generate_sh_file(args.file_name, len(jobs))
+  # generate_sh_file(args.file_name, len(jobs))
