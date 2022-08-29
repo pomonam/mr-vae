e23996fe67ef53c1e6c9a3713003c07d6d52186e
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-29 19:13:25 -0400

    Revert back model

---
+++ experiments/binary_image/sweep_scripts/create_hyper_sweep.py
@@ -0,0 +1,57 @@
+import argparse
+
+from experiments.array_utils import generate_job_strings
+from experiments.array_utils import generate_sh_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file_name", type=str, default="hyper_sweep")
+parser.add_argument(
+    "--experiment_name",
+    type=str,
+    default="hv_b_image_hyper_sweep")
+
+args = parser.parse_args()
+
+CONV_CONFIG = {
+    "lr": [1e-4],
+    "total_epochs": [1000],
+    "data_name": ["mnist", "omniglot"],
+    "encoder_name": ["conv"],
+    "decoder_name": ["conv"],
+    "schedule": ["monotonic"],
+    "include_shift": [0, 1],
+    "include_residual_connection": [0, 1],
+    "include_sigmoid_activation": [0, 1],
+    "preprocess_beta": [0, 1],
+    "include_layer_norm": [0, 1],
+    "include_output_stem": [0, 1],
+}
+
+RENSET_CONFIG = {
+    "lr": [1e-4],
+    "total_epochs": [1000],
+    "data_name": ["mnist", "omniglot"],
+    "encoder_name": ["resnet"],
+    "decoder_name": ["resnet"],
+    "schedule": ["monotonic"],
+    "include_shift": [0, 1],
+    "include_residual_connection": [0, 1],
+    "include_sigmoid_activation": [0, 1],
+    "preprocess_beta": [0, 1],
+    "include_layer_norm": [0, 1],
+    "include_output_stem": [0, 1],
+}
+
+if __name__ == "__main__":
+  jobs = generate_job_strings(
+      CONV_CONFIG,
+      command_template="python hyper_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  jobs += ["\n"]
+  jobs += generate_job_strings(
+      RENSET_CONFIG,
+      command_template="python hyper_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  with open(args.file_name, "w") as f:
+    f.writelines(jobs)
+  generate_sh_file(args.file_name, len(jobs))
--- src/models/resblock.py
+++ src/models/resblock.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from src.hyper.layer import HyperLayer
 
 
 class ResBlock(nn.Module):
@@ -12,6 +13,24 @@ class ResBlock(nn.Module):
         nn.BatchNorm2d(channels),
         nn.ReLU(),
         nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(channels),
+    )
+
+  def forward(self, x: torch.tensor) -> torch.Tensor:
+    return x + self.conv_block(x)
+
+
+class HyperResBlock(nn.Module):
+
+  def __init__(self, channels, hyper_cfg):
+    nn.Module.__init__(self)
+
+    self.conv_block = nn.Sequential(
+        nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(channels),
+        nn.ReLU(),
+        nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1),
+        HyperLayer(channels, hyper_cfg),
         nn.BatchNorm2d(channels),
     )
 
