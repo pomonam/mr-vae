306be0af8bdecf5de02a488a80a427cda3ab95ce
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-30 09:50:25 -0400

    Update LR schedule - finalize experiments

--- experiments/binary_image/scripts/create_baseline_jobs.py
+++ experiments/binary_image/scripts/create_baseline_jobs.py
@@ -1,65 +1,79 @@
 import argparse
-
+import numpy as np
 from experiments.array_utils import generate_job_strings
 from experiments.array_utils import generate_sh_file
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file_name", type=str, default="baseline_sweep")
+parser.add_argument("--file_name", type=str, default="baseline_jobs")
 parser.add_argument(
     "--experiment_name",
     type=str,
-    default="hv_b_image_sweep")
+    default="hv_b_image_jobs")
 
 args = parser.parse_args()
 
 CONV_CONFIG1 = {
-    "lr": [1e-4],
-    "total_epochs": [1000],
-    "data_name": ["mnist", "omniglot"],
+    "lr": [0.001],
+    "total_epochs": [200],
+    "data_name": ["mnist"],
     "encoder_name": ["conv"],
     "decoder_name": ["conv"],
-    "schedule": ["monotonic"],
-    "beta": [1.]
+    "schedule": ["constant", "monotonic"],
+    "beta": np.logspace(-3, 1, num=20) + [1],
+    "save_final_checkpoint": [1]
 }
 
 CONV_CONFIG2 = {
-    "lr": [3e-05],
-    "total_epochs": [1000],
+    "lr": [0.0001],
+    "total_epochs": [200],
     "data_name": ["omniglot"],
     "encoder_name": ["conv"],
     "decoder_name": ["conv"],
-    "schedule": ["monotonic"],
-    "beta": [1.]
+    "schedule": ["constant", "monotonic"],
+    "beta": np.logspace(-3, 1, num=20) + [1],
+    "save_final_checkpoint": [1]
 }
 
 RENSET_CONFIG1 = {
-    "lr": [1e-3, 3e-4, 1e-4, 3e-5, 1e-5],
-    "total_epochs": [1000],
-    "data_name": ["mnist", "omniglot"],
+    "lr": [0.001],
+    "total_epochs": [200],
+    "data_name": ["mnist"],
     "encoder_name": ["resnet"],
     "decoder_name": ["resnet"],
-    "schedule": ["monotonic"],
-    "beta": [1.]
+    "schedule": ["constant", "monotonic"],
+    "beta": np.logspace(-3, 1, num=20) + [1],
+    "save_final_checkpoint": [1]
 }
 
 RENSET_CONFIG2 = {
-    "lr": [1e-3, 3e-4, 1e-4, 3e-5, 1e-5],
-    "total_epochs": [1000],
-    "data_name": ["mnist", "omniglot"],
+    "lr": [0.001],
+    "total_epochs": [200],
+    "data_name": ["omniglot"],
     "encoder_name": ["resnet"],
     "decoder_name": ["resnet"],
-    "schedule": ["monotonic"],
-    "beta": [1.]
+    "schedule": ["constant", "monotonic"],
+    "beta": np.logspace(-3, 1, num=20) + [1],
+    "save_final_checkpoint": [1]
 }
 
 if __name__ == "__main__":
   jobs = generate_job_strings(
-      CONV_CONFIG,
+      CONV_CONFIG1,
       command_template="python baseline_train.py --experiment_name {} ".format(
           args.experiment_name))
   jobs += ["\n"]
   jobs += generate_job_strings(
-      RENSET_CONFIG,
+      CONV_CONFIG2,
+      command_template="python baseline_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  jobs += ["\n"]
+  jobs = generate_job_strings(
+      RENSET_CONFIG1,
+      command_template="python baseline_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  jobs += ["\n"]
+  jobs += generate_job_strings(
+      RENSET_CONFIG2,
       command_template="python baseline_train.py --experiment_name {} ".format(
           args.experiment_name))
   with open(args.file_name, "w") as f:
