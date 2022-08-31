3fbfced87ce455b9addc7e13bceaf6e789d39b31
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-31 17:22:49 -0400

    Add hyper model for text

--- experiments/text/scripts/create_baseline_jobs.py
+++ experiments/text/scripts/create_baseline_jobs.py
@@ -11,18 +11,51 @@ parser.add_argument("--experiment_name", type=str, default="hv_text_baseline")
 
 args = parser.parse_args()
 
-CONFIG = {
-    "lr": [1.],
-    "total_epochs": [50],
-    "data_name": ["yahoo", "yelp"],
+PTB_CONFIG = {
+    "lr": [0.0003],
+    "data_name": ["ptb"],
+    "total_epochs": [100],
+    "decoder_name": ["lstm", "trans"],
     "schedule": ["monotonic", "constant"],
     "beta": np.logspace(-3, 1, num=20),
     "save_final_checkpoint": [1]
 }
 
+
+YAH_CONFIG1 = {
+    "lr": [0.0003],
+    "data_name": ["yahoo"],
+    "total_epochs": [100],
+    "decoder_name": ["lstm"],
+    "schedule": ["monotonic", "constant"],
+    "beta": np.logspace(-3, 1, num=20),
+    "save_final_checkpoint": [1]
+}
+
+YAH_CONFIG2 = {
+    "lr": [0.001],
+    "data_name": ["yahoo"],
+    "total_epochs": [100],
+    "decoder_name": ["trans"],
+    "schedule": ["monotonic", "constant"],
+    "beta": np.logspace(-3, 1, num=20),
+    "save_final_checkpoint": [1]
+}
+
+
 if __name__ == "__main__":
   jobs = generate_job_strings(
-      CONFIG,
+      PTB_CONFIG,
+      command_template="python baseline_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  jobs += ["\n"]
+  jobs += generate_job_strings(
+      YAH_CONFIG1,
+      command_template="python baseline_train.py --experiment_name {} ".format(
+          args.experiment_name))
+  jobs += ["\n"]
+  jobs += generate_job_strings(
+      YAH_CONFIG2,
       command_template="python baseline_train.py --experiment_name {} ".format(
           args.experiment_name))
   with open(args.file_name, "w") as f:
