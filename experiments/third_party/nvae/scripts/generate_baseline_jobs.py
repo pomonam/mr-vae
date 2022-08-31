d162c39e9faa41c6deaaf1711668712f955503c6
pomonam <juhan.bae@mail.utoronto.ca>
2022-08-31 03:59:55 -0400

    NVAE simple model

--- experiments/third_party/nvae/scripts/generate_baseline_jobs.py
+++ experiments/third_party/nvae/scripts/generate_baseline_jobs.py
@@ -4,22 +4,22 @@ from experiments.array_utils import generate_job_strings
 from experiments.array_utils import generate_sh_file
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file_name", type=str, default="baseline_binary_jobs")
+parser.add_argument("--file_name", type=str, default="baseline_jobs")
 parser.add_argument(
-    "--experiment_name", type=str, default="nvae_binary_train_baseline")
+    "--experiment_name", type=str, default="nvae_baseline_jobs")
 
 args = parser.parse_args()
 
 CONFIG = {
-    "dataset": ["mnist", "omniglot"],
-    "kl_fixed": np.logspace(-3, 1, num=20),
+    "dataset": ["mnist", "omniglot", "cifar10", "celeba_64"],
+    "kl_fixed": list(np.logspace(-3, 1, num=20)) + list([1]),
 }
 
 if __name__ == "__main__":
-  base_str = "python new_train.py --data ../../../logs/data --root checkpoints/ --batch_size 200 " \
-             "--epochs 400 --num_latent_scales 2 --num_groups_per_scale 10 --num_postprocess_cells 3 --num_preprocess_cells 3 " \
+  base_str = "python new_train.py --data ../../../logs/data --root checkpoints/ --batch_size 32 " \
+             "--epochs 200 --num_latent_scales 2 --num_groups_per_scale 10 --num_postprocess_cells 3 --num_preprocess_cells 3 " \
              "--num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 --num_latent_per_group 20 --num_preprocess_blocks 2 " \
-             "--num_postprocess_blocks 2 --weight_decay_norm 1e-2 --num_channels_enc 32 --num_channels_dec 32 --num_nf 0 " \
+             "--num_postprocess_blocks 2 --weight_decay_norm 1e-2 --num_channels_enc 64 --num_channels_dec 64 --num_nf 0 " \
              "--ada_groups --use_se --res_dist --fast_adamax "
   jobs = generate_job_strings(
       CONFIG,
@@ -27,4 +27,3 @@ if __name__ == "__main__":
       "--experiment_name {} ".format(args.experiment_name))
   with open(args.file_name, "w") as f:
     f.writelines(jobs)
-  # generate_sh_file(args.file_name, len(jobs))
