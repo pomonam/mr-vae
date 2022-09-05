ac0e0b8e5689cdc402b45372a6fa8828f63fce4c
Juhan Bae <juhan.bae@mail.utoronto.ca>
2022-09-05 19:47:12 -0400

    Add backbone for tests

--- tests/test_binary_image.py
+++ tests/test_binary_image.py
@@ -3,6 +3,8 @@ import unittest
 import torch
 
 from experiments.binary_image.baseline_train import build_model
+from experiments.binary_image.hyper_train import build_model as hyper_build_model
+from src.config import HyperConfig
 from tests.utils import summary
 
 
@@ -13,9 +15,22 @@ class TestBinaryImage(unittest.TestCase):
     self.conv_model = build_model("conv", "conv", device)
     self.resnet_model = build_model("resnet", "resnet", device)
 
+    cfg = HyperConfig(None)
+    cfg.shared_preprocess = True
+    cfg.param_type = "pre_bn"
+    cfg.layer_type = "sig_gate"
+    cfg.block_type = "mlp"
+    cfg.apply_zero_init = True
+    cfg.apply_norm_layers = False
+    cfg.include_output_stem = False
+
+    self.hyper_conv_model = hyper_build_model("conv", "conv", cfg, device)
+    self.hyper_resnet_model = hyper_build_model("resnet", "resnet", cfg, device)
+
   def test_summary(self):
     print(summary(self.conv_model, (1, 28, 28), device="cpu"))
     print(summary(self.resnet_model, (1, 28, 28), device="cpu"))
+    # print(summary(self.hyper_resnet_model, (1, 28, 28), device="cpu"))
 
   def test_forward(self):
     x = torch.randn(2, 1, 28, 28)
