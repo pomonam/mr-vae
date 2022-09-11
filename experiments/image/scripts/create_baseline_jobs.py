2d6dc07bbc62d381a42e675acde927d569f007a1
Juhan Bae <juhan.bae@mail.utoronto.ca>
2022-09-11 17:44:36 -0400

    Add VQVAE baseline scripts

--- experiments/image/scripts/create_baseline_jobs.py
+++ experiments/image/scripts/create_baseline_jobs.py
@@ -8,7 +8,7 @@ from experiments.array_utils import generate_sh_file
 parser = argparse.ArgumentParser()
 parser.add_argument("--file_name", type=str, default="baseline_jobs")
 parser.add_argument(
-    "--experiment_name", type=str, default="hvae_image_jobs_final")
+    "--experiment_name", type=str, default="hvae_image_jobs_resnet_final")
 
 args = parser.parse_args()
 
