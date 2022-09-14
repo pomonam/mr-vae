74e9827199b44ae1d948679a7c2ab737a6e89e12
Michael Ruan <michael.ruan@mail.utoronto.ca>
2022-09-13 20:17:02 -0400

    bash updates

---
+++ tests/theory/overnight3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 1.0 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.3 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.01 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.03 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.001 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.003 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.0001 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.0003 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.00001 --beta 1.833e-2
+python linear_train.py --experiment_name linear_vae_beta_1.833e-2 --lr 0.00003 --beta 1.833e-2
+
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 1.0 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.3 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.01 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.03 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.001 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.003 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.0001 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.0003 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.00001 --beta 2.976e-2
+python linear_train.py --experiment_name linear_vae_beta_2.976e-2 --lr 0.00003 --beta 2.976e-2
+
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 1.0 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.3 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.01 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.03 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.001 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.003 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.0001 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.0003 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.00001 --beta 4.833e-2
+python linear_train.py --experiment_name linear_vae_beta_4.833e-2 --lr 0.00003 --beta 4.833e-2
