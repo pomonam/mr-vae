4b870fe6bca4750fcbd11d1a3a56b1c863c81ad4
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-15 04:32:18 -0400

    Fix nvae scripts

--- experiments/binary_image/nas_sweep_scripts/run_bn_sweep.sh
+++ experiments/binary_image/nas_sweep_scripts/run_bn_sweep.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J test
 #SBATCH --gres=gpu:1
-#SBATCH --mem=8GB
-#SBATCH --partition=t4v1,p100,t4v2,rtx6000
-#SBATCH --qos=normal
+#SBATCH -p gpu
+#SBATCH --mem=16GB
 #SBATCH --export=ALL
 #SBATCH --array=0-56%56
 #SBATCH --output=temp/array-%A_%a.out
-#SBATCH -c 4
+#SBATCH --cpus-per-task=8
 
 . $HOME/envs/hvae_env
 export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
@@ -16,5 +14,5 @@ export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
 IFS=$'\n' read -d '' -r -a lines < bn_sweep
 cd ..
 
-echo ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
-eval ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
+echo ${lines[SLURM_ARRAY_TASK_ID]}
+eval ${lines[SLURM_ARRAY_TASK_ID]}
