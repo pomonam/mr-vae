78f3404502923126cb2282e2c4fe462185706668
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-15 04:31:48 -0400

    Fix nvae scripts

--- experiments/binary_image/nas_sweep_scripts/create_bn_type_sweep.py
+++ experiments/binary_image/nas_sweep_scripts/create_bn_type_sweep.py
@@ -6,24 +6,22 @@ from experiments.array_utils import generate_sh_file
 parser = argparse.ArgumentParser()
 parser.add_argument("--file_name", type=str, default="bn_sweep")
 parser.add_argument(
-    "--experiment_name", type=str, default="hvae_bimage_nas_sweep_bn_type_v4")
+    "--experiment_name", type=str, default="hvae_bimage_nas_sweep_bn_type_v14")
 
 args = parser.parse_args()
 
 CONFIG = {
-    "lr": [3e-3, 1e-3, 3e-4, 1e-4],
+    "lr": [1e-2, 3e-3, 1e-3, 3e-4, 1e-4, 3e-5, 1e-5],
     "total_epochs": [200],
     "data_name": ["mnist", "omniglot"],
     "encoder_name": ["conv"],
     "decoder_name": ["conv"],
-    "shared_preprocess": [0],
-    "layer_type": ["sig_gate"],
-    "param_type": ["post_act"],
-    "apply_zero_init": [0],
+    "encoder_layer_type": ["sig_gate"],
+    "decoder_layer_type": ["sqrt_gate"],
     "block_type": ["linear"],
     "norm_type": ["none", "scale_shift"],
-    "apply_bn_tracking": [0, 1],
-    "apply_bn_calibrate": [0, 1],
+    "apply_bn_tracking": [1],
+    "apply_bn_calibrate": [0],
     "apply_bn_replace": [0, 1],
 }
 
--- experiments/binary_image/nas_sweep_scripts/run_bn_sweep.sh
+++ experiments/binary_image/nas_sweep_scripts/run_bn_sweep.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=t4v1,p100,t4v2,rtx6000
 #SBATCH --qos=normal
 #SBATCH --export=ALL
-#SBATCH --array=0-128%128
+#SBATCH --array=0-56%56
 #SBATCH --output=temp/array-%A_%a.out
 #SBATCH -c 4
 
