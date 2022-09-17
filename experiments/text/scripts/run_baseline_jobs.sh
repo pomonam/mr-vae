781202487daaa7cf819394d5af5a1e5f348eb395
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-17 12:07:05 -0400

    Only use P100

--- experiments/text/scripts/run_baseline_jobs.sh
+++ experiments/text/scripts/run_baseline_jobs.sh
@@ -3,7 +3,7 @@
 #SBATCH -J test
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8GB
-#SBATCH --partition=t4v1,p100,t4v2,rtx6000
+#SBATCH --partition=p100
 #SBATCH --qos=normal
 #SBATCH --export=ALL
 #SBATCH --array=0-122%122
