2dcd5586a9cd41b9b94e9ad12413b5f88714eee9
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-17 12:06:30 -0400

    text constant jobs

---
+++ experiments/text/scripts/run_crashed_jobs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -J test
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8GB
+#SBATCH --partition=p100
+#SBATCH --qos=normal
+#SBATCH --export=ALL
+#SBATCH --array=0-17%17
+#SBATCH --output=temp/array-%A_%a.out
+#SBATCH -c 4
+
+. $HOME/envs/hvae_env
+export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
+
+IFS=$'\n' read -d '' -r -a lines < crashed_jobs
+cd ..
+
+echo ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
+eval ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
