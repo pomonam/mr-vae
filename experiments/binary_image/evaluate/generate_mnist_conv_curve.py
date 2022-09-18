31422a2533e01415ae7912f416fb94666eb680db
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-18 01:02:51 -0400

    clean up old mess and ready to run again

--- experiments/binary_image/evaluate/generate_mnist_conv_curve.py
+++ experiments/binary_image/evaluate/generate_mnist_conv_curve.py
@@ -1,145 +1,90 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from tueplots import bundles
 from tueplots import cycler
 from tueplots import markers
 from tueplots.constants.color import palettes
 from tueplots.constants.color import rgb
 
-from experiments.wandb_utils import init_api
+from experiments.binary_image.evaluate.utils import get_baseline_rd
+from experiments.binary_image.evaluate.utils import get_hyper_rd
 
 ENTITY = "bae-group"
-BASELINE_NAME = "hvae_bimage_jobs_v9"
-HYPER_NAME = "hvae_bimage_hyper_sweep_v10"
-ID = "3vqmpqxn"
-
-
-def get_summary(summary, test=True):
-  if test:
-    beta_to_rate = dict(
-        zip(summary["test/sample_lst"], summary["test/rate_lst"]))
-    beta_to_dist = dict(
-        zip(summary["test/sample_lst"], summary["test/dist_lst"]))
-    beta_to_elbo = dict(
-        zip(summary["test/sample_lst"], summary["test/loss_lst"]))
-  else:
-    beta_to_rate = dict(
-        zip(summary["train_eval/sample_lst"], summary["train_eval/rate_lst"]))
-    beta_to_dist = dict(
-        zip(summary["train_eval/sample_lst"], summary["train_eval/dist_lst"]))
-    beta_to_elbo = dict(
-        zip(summary["train_eval/sample_lst"], summary["train_eval/loss_lst"]))
-  return beta_to_rate, beta_to_dist, beta_to_elbo
-
-
-def get_baseline_summary(config_lst,
-                         summary_lst,
-                         schedule="monotonic",
-                         test=True):
-  beta_to_rate = {}
-  beta_to_dist = {}
-  beta_to_elbo = {}
-
-  for i, c in enumerate(config_lst):
-    if c["schedule"] == schedule and c["data_name"] == "mnist" and c[
-        "encoder_name"] == "conv":
-      if test:
-        beta_to_rate[c["beta"]] = summary_lst[i]["test/rate"]
-        beta_to_dist[c["beta"]] = summary_lst[i]["test/distortion"]
-        beta_to_elbo[c["beta"]] = summary_lst[i]["test/loss"]
-      else:
-        beta_to_rate[c["beta"]] = summary_lst[i]["train_eval/rate"]
-        beta_to_dist[c["beta"]] = summary_lst[i]["train_eval/distortion"]
-        beta_to_elbo[c["beta"]] = summary_lst[i]["train_eval/loss"]
-  sorted_beta_to_rate = dict(
-      sorted(beta_to_rate.items(), key=lambda item: item[0]))
-  sorted_beta_to_dist = dict(
-      sorted(beta_to_dist.items(), key=lambda item: item[0]))
-  sorted_beta_to_elbo = dict(
-      sorted(beta_to_elbo.items(), key=lambda item: item[0]))
-  return sorted_beta_to_rate, sorted_beta_to_dist, sorted_beta_to_elbo
-
-
-def get_baseline_rd(experiment_name, schedule="cyclic", test=True):
-  api = init_api()
-  runs = api.runs(ENTITY + "/" + experiment_name)
-
-  summary_list, config_list, name_list = [], [], []
-  for run in runs:
-    if run.state == "finished":
-      summary_list.append(run.summary._json_dict)
-      config_list.append(
-          {k: v for k, v in run.config.items() if not k.startswith("_")})
-      name_list.append(run.name)
-
-  rate_dict, dist_dict, elbo_dict = get_baseline_summary(config_list,
-                                                         summary_list,
-                                                         schedule=schedule,
-                                                         test=test)
-  keys = rate_dict.keys()
-  values = zip(rate_dict.values(), dist_dict.values())
-  combined_dict = dict(zip(keys, values))
-
-  rate = np.array([c[0] for c in combined_dict.values()])
-  dist = np.array([c[1] for c in combined_dict.values()])
-  return rate, dist
+BASELINE_NAME = "hvae_bimage_jobs_final"
+HYPER_NAME = "hvae_bimage_nas_sweep_block_type_v100"
 
 
 def main():
   plt.rcParams.update({"figure.dpi": 300})
-  plt.rcParams.update(bundles.aistats2022(column="full"))
+  plt.rcParams.update(bundles.neurips2022(ncols=1, nrows=1))
   plt.rcParams.update(cycler.cycler(color=palettes.tue_plot))
   plt.rcParams.update(markers.inverted())
 
-  api = init_api()
-  runs = api.runs(ENTITY + "/" + HYPER_NAME)
-
-  rate, dist = get_baseline_rd(BASELINE_NAME,
-                               schedule="monotonic", arc_name="resnet", test=True)
+  # rate, dist = get_baseline_rd(ENTITY, BASELINE_NAME, data_name="mnist",
+  #                              schedule="monotonic", arc_name="resnet", test=True)
   # plt.plot([0], [0])
-  plt.plot(
-      rate,
-      dist,
-      label=r"Independent Training",
-      # edgecolors="k",
-      # linewidths=0.5,
-      c=rgb.tue_lightblue)
+  # plt.scatter(
+  #   rate,
+  #   dist,
+  #   label=r"Independent Training",
+  #   edgecolors="k",
+  #   linewidths=0.5,
+  #   c=rgb.tue_lightblue)
+  #
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "15t0p722")
+  # plt.plot(rate, dist, "o-", label="Linear", linewidth=1.5)
+  #
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "o5ejijdu")
+  # plt.plot(rate, dist, "o-", label="MLP", linewidth=1.5)
+  #
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "1skahjue")
+  # plt.plot(rate, dist, "o-", label="MLP (shared weights)", linewidth=1.5)
+  #
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "27je3kfn")
+  # plt.plot(rate, dist, "o-", label="Large MLP", linewidth=1.5)
+  #
+  # plt.xlim(0, 120)
+  # plt.ylim(15, 120)
+  #
+  # plt.xlabel("Rate")
+  # plt.ylabel("Distortion")
+  # plt.title("MNIST Dataset")
+  # plt.grid()
+  # plt.legend()
+  # plt.show()
+  # plt.clf()
 
-  summary_list, config_list, name_list = [], [], []
-  for run in runs:
-    if run.id == ID:
-      summary_list.append(run.summary._json_dict)
-      config_list.append(
-          {k: v for k, v in run.config.items() if not k.startswith('_')})
-      name_list.append(run.name)
+  rate, dist = get_baseline_rd(ENTITY, BASELINE_NAME, data_name="omniglot",
+                               schedule="monotonic", arc_name="resnet", test=True)
+  plt.plot([0], [0])
+  plt.scatter(
+    rate,
+    dist,
+    label=r"Independent Training",
+    edgecolors="k",
+    linewidths=0.5,
+    c=rgb.tue_lightblue)
 
-  rate_dict, dist_dict, elbo_dict = get_summary(summary_list[0], test=True)
-  keys = rate_dict.keys()
-  values = zip(rate_dict.values(), dist_dict.values())
-  combined_dict = dict(zip(keys, values))
-  rate = np.array([c[0] for c in combined_dict.values()])
-  dist = np.array([c[1] for c in combined_dict.values()])
-  plt.plot(rate, dist, "-", label="Hypernetwork", linewidth=2, c=rgb.tue_ocre)
+  rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "2ffz8kh2")
+  plt.plot(rate, dist, "o-", label="Linear", linewidth=1.5)
 
-  # rate_dict, dist_dict, elbo_dict = get_summary(summary_list[0], test=False)
-  # keys = rate_dict.keys()
-  # values = zip(rate_dict.values(), dist_dict.values())
-  # combined_dict = dict(zip(keys, values))
-  # rate = np.array([c[0] for c in combined_dict.values()])
-  # dist = np.array([c[1] for c in combined_dict.values()])
-  # plt.plot(rate, dist, "o-", label="Hypernetwork", linewidth=2)
+  rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "3024pfuu")
+  plt.plot(rate, dist, "o-", label="MLP", linewidth=1.5)
 
-  plt.xlim(0, 140)
-  plt.ylim(0, 140)
+  rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "38bj0zlb")
+  plt.plot(rate, dist, "o-", label="MLP (shared weights)", linewidth=1.5)
+
+  rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "2l5eu86b")
+  plt.plot(rate, dist, "o-", label="Large MLP", linewidth=1.5)
+
+  plt.xlim(0, 120)
+  plt.ylim(20, 120)
 
   plt.xlabel("Rate")
   plt.ylabel("Distortion")
-
-  plt.title("Rate-Distortion Curve for MNIST")
+  plt.title("Omniglot Dataset")
   plt.legend()
   plt.grid()
   plt.show()
-  # plt.savefig("mnist_rd_curve.pdf")
 
 
 if __name__ == "__main__":
---
+++ experiments/binary_image/evaluate/generate_omniglot_results.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+from tueplots import bundles
+from tueplots import cycler
+from tueplots import markers
+from tueplots.constants.color import palettes
+from tueplots.constants.color import rgb
+
+from experiments.binary_image.evaluate.utils import get_baseline_rd
+from experiments.binary_image.evaluate.utils import get_hyper_rd
+
+ENTITY = "bae-group"
+BASELINE_NAME = "hvae_bimage_jobs_final"
+HYPER_NAME = "hvae_bimage_hyper_sweep_v100"
+
+
+def main():
+  plt.rcParams.update({"figure.dpi": 300})
+  plt.rcParams.update(bundles.neurips2022(ncols=1, nrows=1))
+  plt.rcParams.update(cycler.cycler(color=palettes.tue_plot))
+  plt.rcParams.update(markers.inverted())
+
+  rate, dist = get_baseline_rd(ENTITY, BASELINE_NAME, data_name="omniglot",
+                               schedule="monotonic", arc_name="resnet", test=True)
+  plt.plot([0], [0])
+  plt.scatter(
+    rate,
+    dist,
+    label=r"Independent Training",
+    edgecolors="k",
+    linewidths=0.5,
+    c=rgb.tue_lightblue,
+    marker="v"
+  )
+
+  rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "1qezffsn")
+  plt.plot(rate, dist, "o-", label="HC-VAE-1", linewidth=1.5)
+
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "3r60bxur")
+  # plt.plot(rate, dist, "o-", label="HC-VAE-2", linewidth=1.5)
+
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "1skahjue")
+  # plt.plot(rate, dist, "o-", label="MLP (shared weights)", linewidth=1.5)
+  #
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "27je3kfn")
+  # plt.plot(rate, dist, "o-", label="Large MLP", linewidth=1.5)
+
+  plt.xlim(0, 120)
+  plt.ylim(15, 120)
+
+  # plt.xlabel("Rate")
+  # plt.ylabel("Distortion")
+  # plt.title("MNIST Dataset")
+  # plt.grid()
+  # plt.legend()
+  # plt.show()
+  # plt.clf()
+
+  # rate, dist = get_baseline_rd(ENTITY, BASELINE_NAME, data_name="omniglot",
+  #                              schedule="monotonic", arc_name="conv", test=True)
+  # plt.plot([0], [0])
+  # plt.scatter(
+  #   rate,
+  #   dist,
+  #   label=r"Independent Training",
+  #   edgecolors="k",
+  #   linewidths=0.5,
+  #   c=rgb.tue_lightblue,
+  #   marker="^"
+  # )
+  #
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "135cx5xb")
+  # plt.plot(rate, dist, "o-", label="HC-VAE-1", linewidth=1.5)
+
+  # rate, dist = get_hyper_rd(ENTITY, HYPER_NAME, "1i04ot2m")
+  # plt.plot(rate, dist, "o-", label="HC-VAE-2", linewidth=1.5)
+
+  plt.xlim(0, 120)
+  plt.ylim(20, 120)
+
+  plt.xlabel("Rate")
+  plt.ylabel("Distortion")
+  plt.title("MNIST Dataset")
+  plt.legend()
+  plt.grid()
+  plt.show()
+
+
+if __name__ == "__main__":
+  main()
