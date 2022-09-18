f2872da87f6c9f563ce8024d43b4e36bb3662d6d
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-18 01:07:16 -0400

    Add missing

--- experiments/binary_image/sweep_scripts/create_hyper_sweep.py
+++ experiments/binary_image/sweep_scripts/create_hyper_sweep.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 CONV_CONFIG = {
     "lr": [1e-2, 3e-3, 1e-3, 3e-4, 1e-4, 3e-5, 1e-5],
     "total_epochs": [200],
-    "data_name": ["mnist"],
+    "data_name": ["omniglot"],
     "encoder_name": ["conv"],
     "decoder_name": ["conv"],
     "hyper_config_summary": ["linear_default", "linear_ss", "mlp_default"]
@@ -33,11 +33,11 @@ if __name__ == "__main__":
       CONV_CONFIG,
       command_template="python hyper_train.py --experiment_name {} ".format(
           args.experiment_name))
-  jobs += ["\n"]
-  jobs += generate_job_strings(
-      RENSET_CONFIG,
-      command_template="python hyper_train.py --experiment_name {} ".format(
-          args.experiment_name))
+  # jobs += ["\n"]
+  # jobs += generate_job_strings(
+  #     RENSET_CONFIG,
+  #     command_template="python hyper_train.py --experiment_name {} ".format(
+  #         args.experiment_name))
   with open(args.file_name, "w") as f:
     f.writelines(jobs)
-  # generate_sh_file(args.file_name, len(jobs) - 1)
+  generate_sh_file(args.file_name, len(jobs), cluster_name="q")
