af05818e6492f86569f8f436818e7941240f19dd
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-18 01:07:16 -0400

    Add missing

--- experiments/binary_image/sweep_scripts/run_hyper_sweep.sh
+++ experiments/binary_image/sweep_scripts/run_hyper_sweep.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J test
 #SBATCH --gres=gpu:1
-#SBATCH --mem=8GB
-#SBATCH --partition=p100
-#SBATCH --qos=normal
+#SBATCH -p gpu
+#SBATCH --mem=16GB
 #SBATCH --export=ALL
-#SBATCH --array=0-63%63
+#SBATCH --array=0-21%21
 #SBATCH --output=temp/array-%A_%a.out
-#SBATCH -c 4
+#SBATCH --cpus-per-task=8
 
 . $HOME/envs/hvae_env
 export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
@@ -16,5 +14,5 @@ export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
 IFS=$'\n' read -d '' -r -a lines < hyper_sweep
 cd ..
 
-echo ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
-eval ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
+echo ${lines[SLURM_ARRAY_TASK_ID]}
+eval ${lines[SLURM_ARRAY_TASK_ID]}
