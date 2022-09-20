4a7d8e690b5b905dbf85095a9dea023fc28273fe
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-19 23:09:26 -0400

    text deadline queue

--- experiments/text/sweep_scripts/run_hyper_sweep.sh
+++ experiments/text/sweep_scripts/run_hyper_sweep.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH -N 1
+#SBATCH -J test
 #SBATCH --gres=gpu:1
-#SBATCH -p gpu
-#SBATCH --mem=16GB
+#SBATCH --mem=8GB
+#SBATCH --partition=t4v1,p100,t4v2
+#SBATCH --account=deadline
+#SBATCH --qos=deadline
 #SBATCH --export=ALL
 #SBATCH --array=0-20%20
 #SBATCH --output=temp/array-%A_%a.out
-#SBATCH --cpus-per-task=8
+#SBATCH -c 4
 
 . $HOME/envs/hvae_env
 export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
@@ -14,5 +17,5 @@ export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
 IFS=$'\n' read -d '' -r -a lines < hyper_sweep
 cd ..
 
-echo ${lines[SLURM_ARRAY_TASK_ID]}
-eval ${lines[SLURM_ARRAY_TASK_ID]}
+echo ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
+eval ${lines[SLURM_ARRAY_TASK_ID]} --checkpoint_dir /checkpoint/${USER}/${SLURM_JOB_ID}
