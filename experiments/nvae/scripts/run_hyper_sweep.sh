4a76ba739d64937c8f2eb6c2aaf5b24ff63b5177
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-19 22:02:50 -0400

    NVAE archi edits

--- experiments/nvae/scripts/run_hyper_sweep.sh
+++ experiments/nvae/scripts/run_hyper_sweep.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
-#SBATCH -p ml
-#SBATCH --mem=40GB
+#SBATCH -p gpu
+#SBATCH --mem=16GB
 #SBATCH --export=ALL
 #SBATCH --array=0-6%6
 #SBATCH --output=temp/array-%A_%a.out
-#SBATCH --nodelist=sonata1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 
 . $HOME/envs/hvae_env
 export PYTHONPATH=$HOME/codes/hyper-vae:$PYTHONPATH
-export PYTHONPATH=$HOME/codes/hyper-vae/experiments/nvae:$PYTHONPATH
 
 IFS=$'\n' read -d '' -r -a lines < hyper_sweep
 cd ..
 
-echo ${lines[SLURM_ARRAY_TASK_ID]} --root checkpoints/${SLURM_JOB_ID}
-eval ${lines[SLURM_ARRAY_TASK_ID]} --root checkpoints/${SLURM_JOB_ID}
+echo ${lines[SLURM_ARRAY_TASK_ID]}
+eval ${lines[SLURM_ARRAY_TASK_ID]}
