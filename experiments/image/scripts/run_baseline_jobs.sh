2e4dd3fb71c09f53ef0c2ae287c03216eada1304
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-21 19:54:12 -0400

    NVAE ddp implementation

--- experiments/image/scripts/run_baseline_jobs.sh
+++ experiments/image/scripts/run_baseline_jobs.sh
@@ -3,11 +3,10 @@
 #SBATCH -J test
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8GB
-#SBATCH --partition=t4v1,p100,t4v2
-#SBATCH --account=deadline
-#SBATCH --qos=deadline
+#SBATCH --partition=t4v1,p100,t4v2,rtx6000
+#SBATCH --qos=normal
 #SBATCH --export=ALL
-#SBATCH --array=0-30%30
+#SBATCH --array=0-185%185
 #SBATCH --output=temp/array-%A_%a.out
 #SBATCH -c 4
 
