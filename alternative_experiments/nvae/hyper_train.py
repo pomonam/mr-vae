9703d594ed3d473b468c75407f44b18e817854f2
pomonam <juhan.bae@mail.utoronto.ca>
2022-09-22 04:16:51 -0400

    modify code to run on slurm

--- alternative_experiments/nvae/hyper_train.py
+++ alternative_experiments/nvae/hyper_train.py
@@ -83,7 +83,7 @@ def main(args):
     writer = utils.Writer(args.global_rank, args.save)
 
     # Get data loaders.
-    train_queue, valid_queue, num_classes = datasets.get_loaders(args)
+    train_queue, valid_queue, num_classes = datasets.get_loaders_eval(args.dataset, args)
     args.num_total_iter = len(train_queue) * args.epochs
     warmup_iters = len(train_queue) * args.warmup_epochs
     swa_start = len(train_queue) * (args.epochs - 1)
